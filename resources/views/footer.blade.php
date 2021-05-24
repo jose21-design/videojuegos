8e72f4a1c895855012ae88b42398c64893e3328b
= <=>
2021-05-24 11:56:05 -0500

    Se creó al auth y sus vistas login, registro. Se agregó footer y header

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,46 @@
+<!-- FOOTER --->
+<footer id="footer" class="pb-4 pt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg ">
+                <div class="row">
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <h5>CONTÁCTANOS</h5>
+                    </div>
+
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <p>5 de mayo #545</p>
+                    </div>
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <p>4741234587</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg ">
+                <div class="row">
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <h5>NUESTRAS REDES SOCIALES</h5>
+                    </div>
+
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <p>5 de mayo #545</p>
+                    </div>
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <p>4741234587</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div id="footerDivider" class="pt-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-12">
+                <p>Copyrigth @ 2021 Game Sell</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END FOOTER -->
