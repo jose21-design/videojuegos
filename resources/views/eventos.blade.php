219413b4a1c82a39d66c97aca1ec771534b74f55
= <=>
2021-05-26 18:25:04 -0500

    Se terminó de hacer la vista de nosotros y se está programando la vista de eventos

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -3,6 +3,77 @@
 @section('content')
 <form method="POST" action="{{ route('eventos') }}">
     @csrf
-    <h1>Eventos</h1>
+    <h1 class="text-center">Próximos Eventos</h1>
+        <div id="nueva" class="container-fluid mt-0 mb-0">
+          <div class="row">
+            <div class="col-lg-6 col-12 pl-0 pr-0">
+              <img src="img/video.png" width="100%">
+            </div>
+            <div class="col-lg-6 pt-4 pb-4">
+              <h2>Gran torneo de fifa 2021</h2>
+              <p>
+                "      Probemos este juego en el próximo torneo que se realizará el día 6 de junio,
+                  inscríbete y aparta tu lugar... <a href="">Más información...</a>
+                
+                " 
+              </p><br>
+              <h2>Clásicos de KOF 2002</h2>
+              <p>
+                "      Las clásicas retas de The King of Fighters en un torneo donde los mejores participan,
+                  inscríbete y aparta tu lugar... <a href="">Más información...</a>
+                
+                "  
+              </p>
+            </div>
+          </div>
+        </div>
+        <section id="cursos" class="mt-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center text-uppercase">
+                        <small>Conoce nuestros</small><h2>Cursos</h2>
+                    </div>
+                </div>
+    
+                <div class="row">
+                    <div class="col-12 col-md-4 col-lg-4 mb-4">
+                        <div class="card" >
+                            <img src="images/html.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">Html</h5>
+                              <p class="card-text">Es un  lenguaje de marcado que se utiliza para el desarrollo de páginas web.</p>
+                              <a href="#" class="btn btn-tecnoeduca" data-toggle="modal" data-target="#htmlModal">Ver curso</a>
+                            </div>
+                          </div>
+                    </div>
+        
+                    <div class="col-12 col-md-4 col-lg-4 mb-4">
+                        <div class="card">
+                            <img src="images/css.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">Css</h5>
+                              <p class="card-text">Hojas de estilo en cascada, es el lenguaje de estilos utilizado para describir la presentación en documentos html.</p>
+                              <a href="#" class="btn btn-tecnoeduca">Ver curso</a>
+                            </div>
+                          </div>
+                    </div>
+        
+                    <div class="col-12 col-md-4 col-lg-4 mb-2">
+                        <div class="card" >
+                            <img src="images/javascript.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">JavaScript</h5>
+                              <p class="card-text">Es un lenguaje de programación ligero, interpetado o compilado con funciones de primera clase.</p>
+                              <a href="#" class="btn btn-tecnoeduca">Ver curso</a>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+    
+            
+         </section>
+        <!-- /Cursos -->
+    
 </form>
 @endsection
--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -3,10 +3,26 @@
 @section('content')
 <form method="POST" action="{{ route('nosotros') }}">
     @csrf
-    <h1>Nosotros</h1>
+    
 
-    <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4679.057194397892!2d-101.93651299042675!3d21.353811988867808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842bd235c63dde59%3A0x926020efd084d812!2sC.Miguel%20Hidalgo%20y%20Costilla%2C%20Centro%2C%20Lagos%20de%20Moreno%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1622042536165!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    <div class="text-center">
+        <img src="img/captura.png" alt="" srcset="">
+        <h1>Acerca de nosotros</h1><br>
     </div>
+    <div class="text-center mt-4">
+        <h3>Misión</h3>
+        <h5>Somos una empresa dedicada a la venta de videojuegos para todo tipo de consolas, procurando un buen servicio y la calidad de nuestros productos</h5><br>
+        <h3>Visión</h3>
+        <h5>Posicionarnos en la mente del consumidor como referente número uno en la región de Lagos de Moreno</h5><br>
+    </div>
+        <div class="text-center mt-4">
+            <h3>Ubicación</h3>
+            <h5>Hidalgo, Centro, Lagos de Moreno</h5>
+            <h5>jdjrv.13@gmail.com</h5>
+            <h4>Horario:</h4>
+            <h5>Lunes - Viernes 12:00 - 08:00 pm / Sábado - Domingo 10:00 am - 07:00 pm</h5>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4679.057194397892!2d-101.93651299042675!3d21.353811988867808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842bd235c63dde59%3A0x926020efd084d812!2sC.Miguel%20Hidalgo%20y%20Costilla%2C%20Centro%2C%20Lagos%20de%20Moreno%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1622042536165!5m2!1ses-419!2smx" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    
 </form>
 @endsection
